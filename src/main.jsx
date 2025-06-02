eabc2747d7a064e140b9f2d3d9ecceca322a78a4
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2025-06-02 14:04:26 +0000

    Router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+//react-router-dom Serve para ligar uma página a outra, como um roteador
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
-    <App />
+    <App /> 
   </StrictMode>,
   </BrowserRouter>
 )
